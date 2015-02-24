8e2db9422953698b759bad9679afe546d8129613
Alasdair Tran <alasdair.tran@gmail.com>
2015-02-25 05:57:03 +1100

    Add some documentation

--- scripts/visualisations.py
+++ scripts/visualisations.py
@@ -1,6 +1,4 @@
-""" Visualisations
-
-"""
+""" Visualisations """
 
 import pandas as pd
 import matplotlib as mpl
@@ -12,7 +10,16 @@ from matplotlib.ticker import FuncFormatter
 
 
 def plot_class_distribution(targets, title):
-    """ Input is a target column of the DataFrame """
+    """ Plot the distribution of the classes.
+        
+        Parameters
+        ----------
+        target : array
+            The target column of the dataset.
+            
+        title : str
+            Title of the plot.
+    """
     
     target_df = DataFrame(targets)
     fig, ax = plt.subplots(figsize=(9, 5))
@@ -24,26 +31,58 @@ def plot_class_distribution(targets, title):
     format_thousands = lambda x, pos: format(int(x), ',')
     ax.get_yaxis().set_major_formatter(FuncFormatter(format_thousands))
     
+    plt.show()
     
-def plot_balanced_accuracy_expected(results, title, classifier_names):
-    """ Results are dict"""
     
-    results = DataFrame(results, index=["Expected Balanced Accuracy"])
-    results = results.reindex(columns=classifier_names)
+def plot_scores(scores, title, x_label, classifier_names):
+    """ Make a barplot of the scores of some performance measure.
+        
+        Parameters
+        ----------
+        scores : dict
+            Where the keys are the classifier names and the values are the scores.
+        
+        title : str
+            Title of the plot.
+            
+        x_label : str
+            Label for the x-axis
+            
+        classifier_names : array
+            List of the names of the classifiers, the order of which will be used
+            to order the bars.
+    """
+    
+    scores = DataFrame(scores, index=[x_label])
+    scores = scores.reindex(columns=classifier_names)
     
     format_as_percent_plot = lambda x, pos: "{:.0f}%".format(x * 100)
     fig, ax = plt.subplots(figsize=(9, 5))
-    results.plot(ax=ax, kind="bar", title=title, fontsize=12)
+    scores.plot(ax=ax, kind="bar", title=title, fontsize=12)
     ax.legend(bbox_to_anchor = (1.5, 0.6))
     ax.set_xticklabels([], rotation=0)
     ax.get_yaxis().set_major_formatter(FuncFormatter(format_as_percent_plot))
     
+    plt.show()
     
     
-def plot_balanced_accuracy_violin(balanced_accuracy_samples, names):
+def plot_balanced_accuracy_violin(balanced_accuracy_samples, classifier_names):
+    """ Make a violin plot of the balanced posterior accuracy.
+        
+        Parameters
+        ----------
+        balanced_accuracy_samples : dict
+            Where the keys are the classifier names and the each value is an array
+            of sample points from which an empirical pdf can be approxmiated.
+            
+        classifier_names : array
+            List of classifier names, the order of which will be used
+            to order the bars.
+    """
+
     fig, ax = plt.subplots(figsize=(11, 9))
-    sns.violinplot(DataFrame(balanced_accuracy_samples, columns=names), ax=ax,
-                   names=[1, 2, 3, 4, 5, 6, 7])
+    sns.violinplot(DataFrame(balanced_accuracy_samples, columns=classifier_names),
+                   ax=ax, names=[1, 2, 3, 4, 5, 6, 7])
     ax.set_title("Posterior Balanced Accuracy")
     
     format_as_percent_plot = lambda x, pos: "{:.0f}%".format(x * 100)
@@ -52,7 +91,41 @@ def plot_balanced_accuracy_violin(balanced_accuracy_samples, names):
     handles = []
     colours = sns.color_palette("husl", 7)
     
-    for c, n in zip(colours, names):
+    for c, n in zip(colours, classifier_names):
         handles.append(mpatches.Patch(color=c, label=n))
     
     ax.legend(handles=handles, bbox_to_anchor = (1.4, 0.6))
+    
+    plt.show()
+    
+    
+def plot_learning_curve(sample_sizes, scores, title):
+    """ Plot the learning curve.
+        
+        Parameters
+        ----------
+        sample_sizes : array
+            The sample sizes in which the classifier is run.
+            
+        scores : array
+            The corresponding score for each sample size.
+            
+        title : str
+            The title of the plot.
+    """
+    
+    fig, ax = plt.subplots(figsize=(9, 5))
+    plt.plot(sample_sizes, scores)
+    ax.set_title(title)
+    
+    format_as_percent_plot = lambda x, pos: "{:.1f}%".format(x * 100)
+    ax.get_yaxis().set_major_formatter(FuncFormatter(format_as_percent_plot))
+    
+    format_thousands = lambda x, pos: format(int(x), ',')
+    ax.get_xaxis().set_major_formatter(FuncFormatter(format_thousands))
+    
+    ax.set_xlabel('Sample Size')
+    ax.set_ylabel('Mean Balanced Accuracy')
+    
+    plt.show()
+

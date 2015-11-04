8e319dc0cc2a7e19afa36073c3af613025eb1f46
yen <lee+github@weiyen.me>
2015-11-04 12:31:00 +1100

    Fixed calculations

--- mclearn/knfst/python/classifier.py
+++ mclearn/knfst/python/classifier.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy as sp
+from itertools import product
 from learn_multiclass_novelty import learn
-from test_multiclass_novelty import score
+from test_multiclass_novelty import score, squared_euclidean_distances
 from sklearn import metrics
 
 class KernelNullSpaceClassifier(object):
@@ -14,6 +15,12 @@ class KernelNullSpaceClassifier(object):
         Implements the histogram intersection kernel.
         '''
         return np.minimum(x, y).sum()
+
+    # def _get_pairwise_min_dist(self):
+    #     dist = np.inf
+    #     for pt1, pt2 in product(self.target_points, self.target_points):
+    #         if pt1 != pt2:
+    #             if 
 
     def _get_metric(self, kernel):
         '''
@@ -37,9 +44,13 @@ class KernelNullSpaceClassifier(object):
     def predict(self, X):
         '''
         Returns +1 if the sample is predicted to be novel, -1 otherwise.
+
+        The threshold is selected between 0 and the minimum distance between
+        two target points.
         '''
         ks = metrics.pairwise_kernels(X=self.X_train, Y=X, metric=self.metric)
         scores = score(self.projection, self.target_points, ks)
+        # min_dist = self._get_pairwise_min_dist()
         prediction = np.array([1 if sc > self.threshold else -1 for sc in scores])
 
-        return prediction
+        return scores
--- mclearn/knfst/python/knfst.py
+++ mclearn/knfst/python/knfst.py
@@ -1,12 +1,7 @@
 import numpy as np
 import scipy as sp
+from rank_nullspace import nullspace
 from sklearn.preprocessing import KernelCenterer
-
-
-def null(a, rtol=1e-5):
-    u, s, v = np.linalg.svd(a)
-    rank = (s > rtol*s[0]).sum()
-    return v[rank:].T.copy()
 
 
 def calculate_knfst(K, labels):
@@ -19,28 +14,38 @@ def calculate_knfst(K, labels):
     n, m = K.shape
     if n != m:
         raise Exception("Kernel matrix must be quadratic")
-        
+    
     centered_k = KernelCenterer().fit_transform(K)
     basis_values, basis_vecs = np.linalg.eig(centered_k)
-  
+
+    idx = basis_values.argsort()[::-1]
+    basis_values = basis_values[idx]
+    basis_vecs = basis_vecs[:, idx]
+
     basis_vecs = basis_vecs[:,basis_values > 1e-12]
     basis_values = basis_values[basis_values > 1e-12]
-    
-    basis_values = np.diag(1/np.sqrt(basis_values))
+ 
+    basis_values = np.diag(1.0/np.sqrt(basis_values))
     basis_vecs  = basis_vecs.dot(basis_values)
-        
+
     L = np.zeros([n,n])
-    for idx in range(len(classes)):
-        L[labels==classes[idx], labels==classes[idx]] = 1/np.sum(labels==classes[idx])
+    for cl in classes:
+        for idx1, x in enumerate(labels == cl):
+            for idx2, y in enumerate(labels == cl):
+                if x and y:
+                    L[idx1, idx2] = 1.0/np.sum(labels==cl)
+    
     M = np.ones([m,m])/m
-    H = (np.eye(m,m)-M).dot(basis_vecs).T.dot(K).dot(np.eye(m,m)-L)
+    H = ((np.eye(m,m)-M).dot(basis_vecs)).T.dot(K).dot(np.eye(n,m)-L)
     
-    T = H.dot(H.T)
+    t_sw = H.dot(H.T)
     
-    eigenvecs = null(T)
+    eigenvecs = nullspace(t_sw)
     if eigenvecs.shape[1] < 1:
-        eigenvals, eigenvecs = np.linalg.eig(T)
+        eigenvals, eigenvecs = np.linalg.eig(t_sw)
         eigenvals = np.diag(eigenvals)
+        min_idx = eigenvals.argsort()[0]
+        eigenvecs = eigenvecs[:, min_idx]
         
     proj = (np.eye(m,m)-M).dot(basis_vecs).dot(eigenvecs)
     return proj

6d9889f45dced08849f6a9160edf74fca0abfd6a
Alasdair Tran <alasdair.tran@gmail.com>
2016-04-11 01:01:57 +0930

    Some processing of UCI data

--- mclearn/tools.py
+++ mclearn/tools.py
@@ -2,6 +2,9 @@
 
 import pickle
 import os
+import sys
+import pandas as pd
+from urllib.request import urlopen
 
 
 def results_exist(pickle_path):
@@ -76,3 +79,67 @@ def save_results(obj, pickle_path):
 
     else:
         print('File already exists.')
+
+
+def log(message, *messages, end='\n'):
+    """ Flush messages immediately in Jupyter notebooks. """
+
+    print(message, *messages, end=end)
+    sys.stdout.flush()
+
+    
+def fetch_data(url, dest, header=None, overwrite=True, process=True,
+    process_fn=None, label=None, sep=','):
+    """ Get data from the internet.
+
+        Parameters
+        ----------
+        url : str
+            The URL where the data can be downloaded.
+        dest : str
+            The destination path where the data will be stored.
+
+        Returns
+        -------
+        data : array
+            The downloaded data.
+    """
+    
+    os.makedirs(os.path.dirname(dest), exist_ok=True)
+
+    if not os.path.exists(dest) or overwrite:
+        with open(dest, 'wb') as f:
+            if header:
+                f.write(str.encode(header + '\n'))
+            if type(url) is str:
+                f.write(urlopen(url).read())
+            elif type(url) is list:
+                for u in url:
+                    f.write(urlopen(u).read())
+    
+    data = pd.read_csv(dest, sep=sep, na_values=['?'])
+    if process:
+        # id is not really a feature
+        if 'id' in data.columns:
+            data = data.drop('id', axis=1)
+        if 'time' in data.columns:
+            data = data.drop('time', axis=1)
+        if 'placeholder' in data.columns:
+            data = data.drop('placeholder', axis=1)
+        
+        # move target to first column
+        data = data[['target']].join(data.drop('target', axis=1))
+
+        if label:
+            data.target = data.target.map(lambda x: label[x])
+        
+        # remove missing
+        data.dropna(axis=0, how='any', inplace=True)
+
+        if process_fn:
+            data = process_fn(data)
+
+        data.to_csv(dest, index=False, float_format='%.12g')
+    
+    return data
+

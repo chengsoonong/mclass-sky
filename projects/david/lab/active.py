3cf2694008f2effaf925fa98356231cd8d3d8038
David Wu <david.wu@outlook.com.au>
2016-10-20 12:16:15 +1100

    Updated implementation of cutting plane active learning procedure and experiments notebook. A work in progress (issue #123).

--- projects/david/lab/active.py
+++ projects/david/lab/active.py
@@ -53,42 +53,112 @@ def initial_polyhedron(X):
 #     print('******** Desired number of points queried ********')    
 #     return w_best
 
+# def active(X, Y, iterations, center='ac', sample=1, M=None,
+#            testing=0):
+
+#     if center == 'ac':
+#         center = accpm.analytic_center
+#     if center == 'random':
+#         center = random_vector
+
+#     (A, b) = initial_polyhedron(X)
+#     i = 0
+
+#     if M == None:
+#         M = 2*A.shape[1]
+
+#     while i < iterations:
+
+#         if testing == 2:
+#             print('\nEntering iteration', i) 
+
+#         w_best = center(A, b)
+#         query_outcome = query(A, b, X, Y, M, 
+#                               sample=sample, w_best=w_best)
+#         (x_chosen, y_chosen) = query_outcome[0]
+#         (X, Y) = query_outcome[1]
+
+#         if testing == 2:
+#             print('    (x_chosen, y_chosen)  is', (x_chosen, y_chosen))
+#             print('    y_chosen * np.dot(w_best, x_chosen) gives',
+#                   y_chosen * np.dot(w_best, x_chosen))
+#             print('\n******** Cutting plane', i+1, 'added ********')
+#             print('    w_best was', w_best)
+
+#         a_cp = (-1 * y_chosen) * x_chosen
+#         norm_a_cp = np.linalg.norm(a_cp)
+#         a_cp = a_cp/norm_a_cp
+#         b_cp = 0  
+#         A = np.vstack((A, a_cp))
+#         b = np.hstack((b, b_cp))
+
+#         if testing == 2:
+#             print('    w_best updated to', accpm.analytic_center(A, b))
+
+#         i = i + 1
+#     if testing == 1 or testing == 2:
+#         print('******** Desired number of points queried ********')    
+#     return w_best
+
 def active(X, Y, iterations, center='ac', sample=1, M=None,
-           testing=0):
+           testing=1):
+
     if center == 'ac':
         center = accpm.analytic_center
     if center == 'random':
         center = random_vector
 
     (A, b) = initial_polyhedron(X)
-    if M == None:
-        M = 2*A.shape[1]
     i = 0
     j = 0
+
+    if M == None:
+        M = A.shape[1]*A.shape[1]
+
     while i < iterations:
+
         if testing == 2:
-            print('\nEntering iteration', j) 
+            print('\nEntering iteration', i) 
+
         w_best = center(A, b)
-        (x_chosen, y_chosen) = query(A, b, X, Y, M, sample=sample, w_best=w_best)
+        query_outcome = query(A, b, X, Y, M, 
+                              sample=sample, w_best=w_best)
+        (x_chosen, y_chosen) = query_outcome[0]
+        (X, Y) = query_outcome[1]
+
         if testing == 2:
             print('    (x_chosen, y_chosen)  is', (x_chosen, y_chosen))
             print('    y_chosen * np.dot(w_best, x_chosen) gives',
                   y_chosen * np.dot(w_best, x_chosen))
-            print('\n******** Cutting plane', i+1, 'added ********')
-            print('    w_best was', w_best)
-        a_cp = (-1 * y_chosen) * x_chosen
-        norm_a_cp = np.linalg.norm(a_cp)
-        a_cp = a_cp/norm_a_cp
-        b_cp = 0  
-        A = np.vstack((A, a_cp))
-        b = np.hstack((b, b_cp))
-        if testing == 2:
-            print('    w_best updated to', accpm.analytic_center(A, b))
-        M = M + 1
+        
+        if y_chosen * np.dot(w_best, x_chosen) <= 0:
+            a_cp = (-1 * y_chosen) * x_chosen
+            norm_a_cp = np.linalg.norm(a_cp)
+            a_cp = a_cp/norm_a_cp
+            b_cp = 0  
+            A = np.vstack((A, a_cp))
+            b = np.hstack((b, b_cp))
+
+            j = j + 1
+
+            if testing == 2:
+                print('\n******** Cutting plane', i+1, 'added ********')
+                print('    w_best was', w_best)
+                print('    w_best updated to', accpm.analytic_center(A, b))
+                print('    a_cp is', a_cp)
+
         i = i + 1
+
+    if testing == 3:
+        return (w_best, j)
+
     if testing == 1 or testing == 2:
-        print('******** Desired number of points queried ********')    
-    return w_best
+        print('******** Desired number of points queried ********')
+        print('   ', j, 'cutting plane(s) generated over', i, 'iterations') 
+        return w_best
+
+    if testing == 0:
+        return w_best
 
 def random_vector(A, b):
     dimension = A.shape[1]
@@ -120,4 +190,7 @@ def query(A, b, X, Y, M, sample=1, w_best=None):
             min_val = current_val
     x_chosen = X[ind]
     y_chosen = Y[ind]
-    return (x_chosen, y_chosen) 
+    X  = np.delete(X, ind, axis=0)
+    Y = np.delete(Y, ind, axis=0)
+
+    return ((x_chosen, y_chosen), (X, Y)) 

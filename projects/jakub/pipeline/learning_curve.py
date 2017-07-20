4254e9deea49d03ed0039047e7897c6f431167a1
Cheng Soon Ong <chengsoon.ong@anu.edu.au>
2017-07-20 16:42:45 +1000

    Minor tidying of learning curve

--- projects/jakub/pipeline/learning_curve.py
+++ projects/jakub/pipeline/learning_curve.py
@@ -1,8 +1,7 @@
 import json
 import sys
-
+import os
 import numpy as np
-
 import model
 
 # Import splitter
@@ -14,6 +13,7 @@ TRAINING_SAMPLES_NUM = 1000000
 TESTING_SAMPLES_NUM = 50000
 
 MAX_AGP = 5000
+START = 500
 STEP = 100
 
 
@@ -46,7 +46,7 @@ def simulate_active_learning(strategy, repeat_cv,
         out_scores = [agp.score(test_X, test_y)]
         print('Done', STEP)
 
-        for s in range(2 * STEP, MAX_AGP + 1, STEP):
+        for s in range(START + STEP, MAX_AGP + 1, STEP):
             recommendations = strategy(agp, train_X)
             all_X = np.append(all_X, train_X[recommendations], 0)
             all_y = np.append(all_y, train_y[recommendations], 0)
@@ -69,7 +69,7 @@ def simulate_active_learning(strategy, repeat_cv,
         out_scores = [agp.score(test_X, test_y)]
         print('Done', STEP)
 
-        for s in range(2 * STEP, MAX_AGP + 1, STEP):
+        for s in range(START + STEP, MAX_AGP + 1, STEP):
             recommendations = strategy(agp, train_X)
             new_train_X = train_X[recommendations]
             new_train_y = train_y[recommendations]
@@ -87,11 +87,28 @@ def simulate_active_learning(strategy, repeat_cv,
 def get_curve(data, strategy, repeat_cv, label):
     print('Starting ', label)
     (train_X, train_y), (test_X, test_y) = data
-    x = list(range(STEP, MAX_AGP+1, STEP))
+    x = list(range(START, MAX_AGP+1, STEP))
     y = simulate_active_learning(strategy, repeat_cv,
                                  train_X[:MAX_AGP], train_y[:MAX_AGP], test_X, test_y)
     print('Finished', label)
     return {'x': x, 'y': y, 'label': label}
+
+
+def json_append(fname, entry):
+    """If output file does not exist, create it.
+    Otherwise append to it (in a list)"""
+    if not os.path.isfile(fname):
+        a = []
+        a.append(entry)
+        with open(fname, mode='w') as f:
+            f.write(json.dumps(a, indent=2))
+    else:
+        with open(fname) as feedsjson:
+            feeds = json.load(feedsjson)
+
+        feeds.append(entry)
+        with open(fname, mode='w') as f:
+            f.write(json.dumps(feeds, indent=2))
 
 
 SETTINGS = [
@@ -100,14 +117,15 @@ SETTINGS = [
         {'strategy': passive_strategy, 'repeat_cv': True, 'label': 'Passive with repeat CV'},
         {'strategy': active_strategy, 'repeat_cv': True, 'label': 'Active with repeat CV'}
     ]
+
+
 def main(path_in, path_out):
     data = splitter.load(path_in)
     data = splitter.split(data, TRAINING_SAMPLES_NUM, TESTING_SAMPLES_NUM)
 
-    curves = [get_curve(data, **params) for params in SETTINGS]
-
-    with open(path_out, 'w') as f:
-        json.dump(curves, f)
+    for params in SETTINGS:
+        curve = get_curve(data, **params)
+        json_append(path_out, curve)
 
 
 if __name__ == '__main__':

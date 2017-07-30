9f6fb5326787ce751efb8196b4679ccd9d6b533d
Jakub Nabaglo <jaknab@me.com>
2017-07-30 10:56:22 +1000

    Feature differences

--- projects/jakub/pipeline/learning_curve.py
+++ projects/jakub/pipeline/learning_curve.py
@@ -2,6 +2,7 @@ import json
 import sys
 
 import numpy as np
+import sklearn.model_selection
 
 import model
 
@@ -50,6 +51,9 @@ def simulate_active_learning(strategy, repeat_cv,
             recommendations = strategy(agp, train_X)
             all_X = np.append(all_X, train_X[recommendations], 0)
             all_y = np.append(all_y, train_y[recommendations], 0)
+            assert all_X.shape[1] == train_X.shape[1]
+            assert len(all_y.shape) == 1 and len(all_X.shape) == 2
+
             train_X = np.delete(train_X, recommendations, 0)
             train_y = np.delete(train_y, recommendations, 0)
 
@@ -86,23 +90,42 @@ def simulate_active_learning(strategy, repeat_cv,
 
 def get_curve(data, strategy, repeat_cv, label):
     print('Starting ', label)
-    (train_X, train_y), (test_X, test_y) = data
-    x = list(range(STEP, MAX_AGP+1, STEP))
-    y = simulate_active_learning(strategy, repeat_cv,
-                                 train_X[:MAX_AGP], train_y[:MAX_AGP], test_X, test_y)
+    X, y = data
+
+    plot_x = list(range(STEP, MAX_AGP+1, STEP))
+    plot_ys = []
+
+    kf = sklearn.model_selection.KFold(n_splits=3)
+    for train_index, test_index in kf.split(X):
+        train_X, test_X = X[train_index], X[test_index]
+
+        train_y, test_y = y[train_index], y[test_index]
+
+        plot_y = simulate_active_learning(
+            strategy, repeat_cv,
+            train_X[:MAX_AGP], train_y[:MAX_AGP], test_X, test_y)
+        plot_ys.append(plot_y)
+        break
+
+    # TODO: Is this necessary or can json.dump handle np.ndarray?
+    avg_plot_y = list(np.mean(plot_ys, axis=0))
+
     print('Finished', label)
-    return {'x': x, 'y': y, 'label': label}
+    return {'x': plot_x, 'y': avg_plot_y, 'label': label}
 
 
 SETTINGS = [
-        {'strategy': passive_strategy, 'repeat_cv': False, 'label': 'Passive without repeat CV'},
-        {'strategy': active_strategy, 'repeat_cv': False, 'label': 'Active without repeat CV'},
-        {'strategy': passive_strategy, 'repeat_cv': True, 'label': 'Passive with repeat CV'},
-        {'strategy': active_strategy, 'repeat_cv': True, 'label': 'Active with repeat CV'}
+        {'strategy': passive_strategy, 'repeat_cv': False, 'label': 'Passive'},# without repeat CV'},
+        {'strategy': active_strategy, 'repeat_cv': False, 'label': 'Active'},# without repeat CV'},
+        # {'strategy': passive_strategy, 'repeat_cv': True, 'label': 'Passive with repeat CV'},
+        # {'strategy': active_strategy, 'repeat_cv': True, 'label': 'Active with repeat CV'}
     ]
 def main(path_in, path_out):
     data = splitter.load(path_in)
-    data = splitter.split(data, TRAINING_SAMPLES_NUM, TESTING_SAMPLES_NUM)
+    # assert data[0].shape[0] > 1000000
+    data, _ = splitter.split(data, data[0].shape[0], 0)
+    X, _ = data
+    X[:,1:] -= X[:,:-1]
 
     curves = [get_curve(data, **params) for params in SETTINGS]
 
